ffc9ad51a011cf8d89caaa75a51f39cd26baf005
Mark Ragno <mark@ragno76.com>
2014-11-21 13:27:50 -0700

    added font-awesome

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="/css/styles.css">
 	<link href='http://fonts.googleapis.com/css?family=Passion+One|Roboto:400,700' rel='stylesheet' type='text/css'>
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 	<title>Paws2Read</title>
 </head>
 <body>
@@ -21,11 +22,11 @@
 	<div class="hero panel" id="hero">
 		<div>
 			<h1>Paws2Read</h1>
-			<!-- <div class="mission">
+			<div class="mission">
 				<div><a href="#involved" class="donate link">Donate</a></div>
 				<h3>Paws 2 Read is a reading program where children read to pet therapy animals and benefit from a non-threatening, fun environment to practice their reading skills, increase their self-esteem, gain a love of reading, and learn about the human-animal connection.</h3>
 				<div><a href="mailto:Paws2Read@Paws2Read.org" class="contact link">Contact</a></div>
-			</div> -->
+			</div>
 		</div>
 	</div>
 
