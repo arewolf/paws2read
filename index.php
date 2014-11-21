aa7929e72bb28d28132322ee3c54aabc90211466
Mark Ragno <mark@ragno76.com>
2014-11-21 11:51:59 -0700

    css changes

--- index.php
+++ index.php
@@ -9,34 +9,41 @@
 	<header>
 			<img src="http://www.paws2read.org/img/logo.png">
 		<nav>
-			<li><a href="">Home</a></li>
-			<li><a href="">About</a></li>
-			<li><a href="">Get Involved</a></li>
-			<li><a href="">Map</a></li>
-			<li><a href="">Schedule</a></li>
+			<a href="#hero">Home</a>
+			<a href="#about">About</a>
+			<a href="#involved">Get Involved</a>
+			<a href="#map">Map</a>
+			<a href="#schedule">Schedule</a>
 		</nav>
 	</header>
 
-	<div class="hero panel">
-		<h1>Paws2Read</h1>
+	<div class="hero panel" id="hero">
+		<div class="hero">
+			<h1>Paws2Read</h1>
+			<div class="mission">
+				<div><a href="#involved" class="donate link">Donate</a></div>
+				<h2>Mission Statement</h2>
+				<div><a href="mailto:Paws2Read@Paws2Read.org" class="contact link">Contact</a></div>
+			</div>
+		</div>
 	</div>
 
-	<div class="about panel">
+	<div class="about panel" id="about">
 		<div class="wordbox">
 		</div>
 	</div>
 
-	<div class="involved panel">
+	<div class="involved panel" id="involved">
 		<div class="wordbox">
 		</div>
 	</div>
 
-	<div class="map panel">
+	<div class="map panel" id="map">
 		<div class="wordbox">
 		</div>
 	</div>
 
-	<div class="schedule panel">
+	<div class="schedule panel" id="schedule">
 		<div class="wordbox">
 		</div>
 	</div>
