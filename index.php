bfb8753af8dd589ea3974862465b558f37e9021c
Russ Wolf <FerrousOxide@Russs-MBP.apollosrc.root>
2014-11-21 10:11:46 -0700

    first main push

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="/css/styles.css">
+	<title>Paws2Read</title>
+</head>
+<body>
+	<header>
+	</header>	
+	<div class="hero panel">
+		<h1>Paws2Read</h1>
+	</div>
+
+	<div class="about panel">
+		<div class="wordbox">
+		</div>
+	</div>
+
+	<div class="donate panel">
+		<div class="wordbox">
+		</div>
+	</div>
+
+	<div class="map panel">
+		<div class="wordbox">
+		</div>
+	</div>
+	<footer>
+	</footer>
+
+
+</body>
+</html>
