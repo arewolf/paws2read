aa3a4e281970fb4560f1760f9e8c996112155990
Mark Ragno <mark@ragno76.com>
2014-11-21 15:25:10 -0700

    final

--- index.php
+++ index.php
@@ -6,29 +6,38 @@
 
 	<link href='http://fonts.googleapis.com/css?family=Passion+One|Roboto:400,700' rel='stylesheet' type='text/css'>
 	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	 var js, fjs = d.getElementsByTagName(s)[0];
+	 if (d.getElementById(id)) return;
+	 js = d.createElement(s); js.id = id;
+	 js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+	 fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
 	<title>Paws2Read</title>
 </head>
 <body>
+	
+
+	<div class="hero panel" id="hero">
 	<header>
-			<img src="http://www.paws2read.org/img/logo.png">
+			<div class="logo">
+				<img src="http://www.paws2read.org/img/logo.png">
+			<div>
 		<nav>
 			<a href="#hero">Home</a>
 			<a href="#about">About</a>
 			<a href="#involved">Get Involved</a>
-			<a href="#map">Map</a>
 			<a href="#schedule">Schedule</a>
+			<a href="mailto:Paws2Read@Paws2Read.org">Contact</a>
+			<div class="fb-like" data-href="https://facebook.com/Paws2Read" data-layout="button" data-action="like" data-show-faces="true" data-share="true"></div>
 		</nav>
 	</header>
-
-	<div class="hero panel" id="hero">
-		<div>
-			<h1>Paws2Read</h1>
-			<div class="mission">
-				<div><a href="#involved" class="donate link">Donate</a></div>
-				<h3>Paws 2 Read is a reading program where children read to pet therapy animals and benefit from a non-threatening, fun environment to practice their reading skills, increase their self-esteem, gain a love of reading, and learn about the human-animal connection.</h3>
-				<div><a href="mailto:Paws2Read@Paws2Read.org" class="contact link">Contact</a></div>
-			</div>
+		<div class="focus">
+			<h1 class="huge"><i class="fa fa-paw"></i> Paws2Read <i class="fa fa-paw"></i></h1>
+			<br>
+			<h1>Come, Sit, and Read!</h1>
+			
 		</div>
 
 	</div>
@@ -41,17 +50,17 @@
 		<h3>Certifications</h3>
 
 			<ul>
-				<li>All teams are fingerprinted.</li>
-				<li>All teams are therapy certified and insured.</li>
-				<li>All therapy animals are certified and safe.</li>
+				<li> All teams are fingerprinted.</li>
+				<li> All teams are therapy certified and insured.</li>
+				<li> All therapy animals are certified and safe.</li>
 			</ul>
 
 		<h3>Community</h3>
 			<ul>
-				<li>1 therapy team impacts over 1,000 children each year.</li>
-				<li>Volunteers currently have donated over 5,000 hours in their communities to help increase reading comprehension in young readers.</li>
-				<li>Research shows that there is a 30% increase in reading fluency over a course of 10 weeks Washington University School of Medicine (Smith and Meehan). </li>
-				<li>On a national test of 4th grade reading proficiency Arizona ranked 46 out of the 50 states.</li>
+				<li> 1 therapy team impacts over 1,000 children each year.</li>
+				<li> Volunteers currently have donated over 5,000 hours in their communities to help increase reading comprehension in young readers.</li>
+				<li> Research shows that there is a 30% increase in reading fluency over a course of 10 weeks Washington University School of Medicine (Smith and Meehan). </li>
+				<li> On a national test of 4th grade reading proficiency Arizona ranked 46 out of the 50 states.</li>
 			</ul>
 		</div>
 		</div>
@@ -62,22 +71,22 @@
 			<h2>GET INVOLVED</h2>
 
 			<ul>
-				<li>Invite Us</li>
+				<li> Invite Us</li>
 					<ul>
-						<li>We'll Come to Your Library or School</li>
-						<li>Invite a Paws 2 Read Team to Speak to Your Corporate or Community Group</li>
+						<li> We'll Come to Your Library or School</li>
+						<li> Invite a Paws 2 Read Team to Speak to Your Corporate or Community Group</li>
 					</ul>
-				<li>Assist Us</li>
+				<li> Assist Us</li>
 					<ul>
-						<li>Join a Paws 2 Read Therapy Team</li>
-						<li>Volunteer. We Would Love Your Help</li>
+						<li> Join a Paws 2 Read Therapy Team</li>
+						<li> Volunteer. We Would Love Your Help</li>
 					</ul>
-				<li>Donate to Us</li>
+				<li> Donate to Us</li>
 					<ul> 
-						<li>Printing or Marketing Services</li>
-						<li>Donate Embroidery Services</li>
-						<li>Design or Web Administration</li>
-						<li>Sponsor a Team for a Year</li>
+						<li> Printing or Marketing Services</li>
+						<li> Donate Embroidery Services</li>
+						<li> Design or Web Administration</li>
+						<li> Sponsor a Team for a Year</li>
 					</ul>
 			</ul>
 		</div>
@@ -85,119 +94,125 @@
 
 
 	<div class="schedule panel" id="schedule">
-		<div class="wordbox">
-			<div>
-				<h3>1st Saturday of the Month</h3>
-
+		<div class="wordbox1 oversized">
+			<h1>SCHEDULE:</h1>
+			<br>
+			<h3>Every Thursday</h3>
 					<ul>
-						<li>Mesa Main</li>
+						<li> Phoenix Mesquite Library</li>
 							<ul>
-								<li>10:30am to 12 noon</li>
-								<li>64 E 1st St Mesa AZ 85201</li>  
-								<li>(480) 644-3100</li>
-								<li><a href="https://www.google.com/maps/place/64+E+1st+St,+Mesa,+AZ+85201/@33.4179877,-111.8288694,16z/data=!4m2!3m1!1s0x872ba7c3b68e4aa7:0x710079f8f8fa024" title="">Get Directions</a></li>
-							</ul><br>
-
-						<li>Mesa Red Mountain</li>
-							<ul>
-								<li>Macaw Special Session: 11:30am to 1pm</li>
-						 		<li>Regular Hours: 2:00pm to 3:30pm</li>
-						  		<li>635 N Power Rd Mesa AZ 85205</li>
-						  		<li>(480) 644-3100</li>
-						  		<li><a href="https://www.google.com/maps/place/635+N+Power+Rd,+Mesa,+AZ+85205/@33.426686,-111.6835281,15z/data=!4m2!3m1!1s0x872baff680a69ab7:0x9c9c74acf7f9e641" title="">Get Directions</a></li>
-							</ul><br>		
-
-						<li>Fountain Hills</li>
-						<ul>
-						 	<li>10:30 am to 12 noon</li>
-						 	<li>12901 N La Montana Dr Fountain Hills AZ 85268</li>
-						 	<li>(602) 652-3000</li>
-						 	<li><a href="https://www.google.com/maps/place/12901+N+La+Montana+Dr,+Fountain+Hills,+AZ+85268/@33.5961018,-111.7166474,14z/data=!4m2!3m1!1s0x872b988f619d3ef7:0x38fe30d8a37300bd" title="">Get Directions</a></li>
-						 </ul>
+								<li> 3:30pm to 4:30pm</li>
+								<li> 4525 E Paradise Village Pkwy N Phoenix AZ 85032</li>
+								<li> (602) 262-4636</li>
+								<li> <a href="https://www.google.com/maps/place/4525+E+Paradise+Village+Pkwy+N,+Phoenix,+AZ+85032/@33.6044908,-111.98076,15z/data=!4m2!3m1!1s0x872b73b960e580df:0x8c15b5a34d4d7e15" title="">Get Directions</a></li>
+							</ul>
 					</ul>
 
+			<h3>1st Saturday of the Month</h3>
+
+					<ul>
+						<li> Mesa Main</li>
+							<ul>
+								<li> 10:30am to 12 noon</li>
+								<li> 64 E 1st St Mesa AZ 85201</li>  
+								<li> (480) 644-3100</li>
+								<li> <a href="https://www.google.com/maps/place/64+E+1st+St,+Mesa,+AZ+85201/@33.4179877,-111.8288694,16z/data=!4m2!3m1!1s0x872ba7c3b68e4aa7:0x710079f8f8fa024" title="">Get Directions</a></li>
+							</ul><br>
+
+						<li> Mesa Red Mountain</li>
+							<ul>
+								<li> Macaw Special Session: 11:30am to 1pm</li>
+						 		<li> Regular Hours: 2:00pm to 3:30pm</li>
+						  		<li> 635 N Power Rd Mesa AZ 85205</li>
+						  		<li> (480) 644-3100</li>
+						  		<li> <a href="https://www.google.com/maps/place/635+N+Power+Rd,+Mesa,+AZ+85205/@33.426686,-111.6835281,15z/data=!4m2!3m1!1s0x872baff680a69ab7:0x9c9c74acf7f9e641" title="">Get Directions</a></li>
+							</ul><br>		
+
+						<li> Fountain Hills</li>
+						<ul>
+						 	<li> 10:30 am to 12 noon</li>
+						 	<li> 12901 N La Montana Dr Fountain Hills AZ 85268</li>
+						 	<li> (602) 652-3000</li>
+						 	<li> <a href="https://www.google.com/maps/place/12901+N+La+Montana+Dr,+Fountain+Hills,+AZ+85268/@33.5961018,-111.7166474,14z/data=!4m2!3m1!1s0x872b988f619d3ef7:0x38fe30d8a37300bd" title="">Get Directions</a></li>
+						 </ul>
+					</ul>
+				<br>
 				<h3>2nd Saturday of the Month</h3>
 
 					<ul>
-						<li>Gilbert South East Regional</li>
+						<li> Gilbert South East Regional</li>
 							<ul>
-								<li>10:30am to 12 noon</li>
-								<li>775 N Greenfield Rd Gilbert AZ 85234</li>
-								<li>(602) 652-3000</li>
-								<li><a href="https://www.google.com/maps/place/775+N+Greenfield+Rd,+Gilbert,+AZ+85234/@33.362639,-111.7347744,15z/data=!4m2!3m1!1s0x872baec279960ec3:0xb0f51b0e9b07aba7" title="">Get Directions</a></li>
+								<li> 10:30am to 12 noon</li>
+								<li> 775 N Greenfield Rd Gilbert AZ 85234</li>
+								<li> (602) 652-3000</li>
+								<li> <a href="https://www.google.com/maps/place/775+N+Greenfield+Rd,+Gilbert,+AZ+85234/@33.362639,-111.7347744,15z/data=!4m2!3m1!1s0x872baec279960ec3:0xb0f51b0e9b07aba7" title="">Get Directions</a></li>
 							</ul>		
-					</ul>
+					</ul></div>
+		<!-- <div class="wordbox"> -->
+			
 				
+				<div class="wordbox oversized"> 
 				<h3>3rd Saturday of the Month</h3>
 
 					<ul>
-						<li>Apache Junction</li>
+						<li> Apache Junction</li>
 							<ul>
-								<li>10:30am to 12 noon</li>
-						 		<li>1177 N Idaho Rd Apache Junction AZ 85119</li>
-						 		<li>(480) 474-8555</li>
-						 		<li><a href="https://www.google.com/maps/place/1177+N+Idaho+Rd,+Apache+Junction,+AZ+85119/@33.4240964,-111.5473975,15z/data=!4m2!3m1!1s0x872bb717c48943c9:0xeb340f969cb8648" title="">Get Directions</a> </li>
+								<li> 10:30am to 12 noon</li>
+						 		<li> 1177 N Idaho Rd Apache Junction AZ 85119</li>
+						 		<li> (480) 474-8555</li>
+						 		<li> <a href="https://www.google.com/maps/place/1177+N+Idaho+Rd,+Apache+Junction,+AZ+85119/@33.4240964,-111.5473975,15z/data=!4m2!3m1!1s0x872bb717c48943c9:0xeb340f969cb8648" title="">Get Directions</a> </li>
 							</ul><br>
 							
-						<li>Fountain Hills</li>
+						<li> Fountain Hills</li>
 							<ul>
-						 		<li>10:30am to 12 noon</li>
-						 		<li>12901 N La Montana Dr Fountain Hills AZ 85268</li>
-						 		<li>(602) 652-3000</li>
-						 		<li><a href="https://www.google.com/maps/place/12901+N+La+Montana+Dr,+Fountain+Hills,+AZ+85268/@33.5961018,-111.7166474,14z/data=!4m2!3m1!1s0x872b988f619d3ef7:0x38fe30d8a37300bd" title="">Get Directions</a></li>		
+						 		<li> 10:30am to 12 noon</li>
+						 		<li> 12901 N La Montana Dr Fountain Hills AZ 85268</li>
+						 		<li> (602) 652-3000</li>
+						 		<li> <a href="https://www.google.com/maps/place/12901+N+La+Montana+Dr,+Fountain+Hills,+AZ+85268/@33.5961018,-111.7166474,14z/data=!4m2!3m1!1s0x872b988f619d3ef7:0x38fe30d8a37300bd" title="">Get Directions</a></li>		
 							</ul><br>
 
-						<li>Peoria</li>
+						<li> Peoria</li>
 						<ul>
-							<li>1pm to 2:30pm</li>
-							<li>8463 W Monroe St Peoria AZ</li>
-							<li>(623) 773-7555</li>
-							<li><a href="https://www.google.com/maps/place/8463+W+Monroe+St,+Peoria,+AZ+85345/@33.5759611,-112.2396144,15z/data=!4m2!3m1!1s0x872b41fdd2de4ceb:0x4fe3fc30c3080844" title="">Get Directions</a></li>
+							<li> 1pm to 2:30pm</li>
+							<li> 8463 W Monroe St Peoria AZ</li>
+							<li> (623) 773-7555</li>
+							<li> <a href="https://www.google.com/maps/place/8463+W+Monroe+St,+Peoria,+AZ+85345/@33.5759611,-112.2396144,15z/data=!4m2!3m1!1s0x872b41fdd2de4ceb:0x4fe3fc30c3080844" title="">Get Directions</a></li>
 						</ul><br>
 
-						<li>Perry</li>
+						<li> Perry</li>
 							<ul>
-						 		<li>10:30am to 12 noon</li>  
-								<li>1965 E Queen Creek Rd Gilbert AZ 85297</li>
-								<li>(602) 652-3000</li>
-								<li><a href="https://www.google.com/maps/place/1965+E+Queen+Creek+Rd,+Gilbert,+AZ+85297/@33.2613876,-111.7416774,15z/data=!4m2!3m1!1s0x872bacbb46c81c0f:0x4f4fa997a3e6cebb" title="">Get Directions</a></li>
+						 		<li> 10:30am to 12 noon</li>  
+								<li> 1965 E Queen Creek Rd Gilbert AZ 85297</li>
+								<li> (602) 652-3000</li>
+								<li> <a href="https://www.google.com/maps/place/1965+E+Queen+Creek+Rd,+Gilbert,+AZ+85297/@33.2613876,-111.7416774,15z/data=!4m2!3m1!1s0x872bacbb46c81c0f:0x4f4fa997a3e6cebb" title="">Get Directions</a></li>
 							</ul>
 					</ul>
 
 				<h3>4th Saturday of the Month</h3>
 
 					<ul>
-						<li>Queen Creek</li>
+						<li> Queen Creek</li>
 							<ul>
-						 		<li>10:30am to 12 noon</li>
-						 		<li>21802 S Ellsworth Rd Queen Creek AZ</li>
-						 		<li>(602) 652-3000</li>
-						 		<li><a href="https://www.google.com/maps/place/21802+S+Ellsworth+Rd,+Queen+Creek,+AZ+85142/@33.2514896,-111.6338183,14z/data=!4m2!3m1!1s0x872a4d3c26f40a4d:0xf536a521bed2c963" title="">Get Directions</a></li>
+						 		<li> 10:30am to 12 noon</li>
+						 		<li> 21802 S Ellsworth Rd Queen Creek AZ</li>
+						 		<li> (602) 652-3000</li>
+						 		<li> <a href="https://www.google.com/maps/place/21802+S+Ellsworth+Rd,+Queen+Creek,+AZ+85142/@33.2514896,-111.6338183,14z/data=!4m2!3m1!1s0x872a4d3c26f40a4d:0xf536a521bed2c963" title="">Get Directions</a></li>
 							</ul><br>
 
-						<li>South Mountain</li>
+						<li> South Mountain</li>
 							<ul>
-								<li>10:30am to 12 noon</li>
-						 		<li>7050 S 24th St Phoenix AZ 85042</li>
-						 		<li>(602) 243-8187</li>
-						 		<li><a href="https://www.google.com/maps/place/7050+S+24th+St,+Phoenix,+AZ+85042/@33.3841706,-112.0291066,14z/data=!4m2!3m1!1s0x872b0fe8b91b3e0d:0x6b03335284df237f" title="">Get Directions</a></li>
+								<li> 10:30am to 12 noon</li>
+						 		<li> 7050 S 24th St Phoenix AZ 85042</li>
+						 		<li> (602) 243-8187</li>
+						 		<li> <a href="https://www.google.com/maps/place/7050+S+24th+St,+Phoenix,+AZ+85042/@33.3841706,-112.0291066,14z/data=!4m2!3m1!1s0x872b0fe8b91b3e0d:0x6b03335284df237f" title="">Get Directions</a></li>
 							</ul>
 					</ul>
-				<h3>Every Thursday</h3>
-					<ul>
-						<li>Phoenix Mesquite Library</li>
-							<ul>
-								<li>3:30pm to 4:30pm</li>
-								<li>4525 E Paradise Village Pkwy N Phoenix AZ 85032</li>
-								<li>(602) 262-4636</li>
-								<li><a href="https://www.google.com/maps/place/4525+E+Paradise+Village+Pkwy+N,+Phoenix,+AZ+85032/@33.6044908,-111.98076,15z/data=!4m2!3m1!1s0x872b73b960e580df:0x8c15b5a34d4d7e15" title="">Get Directions</a></li>
-							</ul>
-					</ul>
-			</div>
+					</div>
+				
 		</div>
 	</div>
 	<footer>
-		<p>Copyright © 2013 Paws2Read.org</p>
+		<p>Copyright © 2014 Paws2Read.org</p>
 	</footer>
 
 </body>
