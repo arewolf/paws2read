132f28cebdc72aa825df78e970e846ee17b7e331
Russ Wolf <FerrousOxide@Russs-MBP.apollosrc.root>
2014-11-21 11:51:08 -0700

    involved

---
+++ involved.php
@@ -0,0 +1,23 @@
+<h2>GET INVOLVED</h2>
+
+<ul>
+	<li>Invite Us</li>
+		<ul>
+			<li>We'll Come to Your Library or School</li>
+			<li>Invite a Paws 2 Read Team to Speak to Your Corporate or Community Group</li>
+		</ul>
+	<li>Assist Us</li>
+		<ul>
+			<li>Join a Paws 2 Read Therapy Team</li>
+			<li>Volunteer. We Would Love Your Help</li>
+		</ul>
+	<li>Donate to Us</li>
+		<ul> 
+			<li>Printing or Marketing Services</li>
+			<li>Donate Embroidery Services</li>
+			<li>Design or Web Administration</li>
+			<li>Sponsor a Team for a Year</li>
+		</ul>
+</ul>
+
+
