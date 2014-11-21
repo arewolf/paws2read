e711aed3fd8e88be09ff421e35a7c98f28156b61
Russ Wolf <FerrousOxide@Russs-MBP.apollosrc.root>
2014-11-21 12:29:09 -0700

    aboutus

---
+++ aboutus.php
@@ -0,0 +1,19 @@
+
+
+<h2>About Us</h2>
+
+<h3>Certifications</h3>
+<ul>
+	<li>All teams are fingerprinted.</li>
+	<li>All teams are therapy certified and insured.</li>
+	<li>All therapy animals are certified and safe.</li>
+</ul>
+
+<h3>Community</h3>
+	<ul>
+		<li>1 therapy team impacts over 1,000 children each year.</li>
+		<li>Volunteers currently have donated over 5,000 hours in their communities to help increase reading comprehension in young readers.</li>
+		<li>Research shows that there is a 30% increase in reading fluency over a course of 10 weeks Washington University School of Medicine (Smith and Meehan). </li>
+		<li>On a national test of 4th grade reading proficiency Arizona ranked 46 out of the 50 states.</li>
+	</ul>
+
